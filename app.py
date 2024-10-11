2d0797fea560b812cf74a53165abc6bba1d64641
sairahann <152267559+sairahann@users.noreply.github.com>
2024-10-11 01:52:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('linkedin_follow.html')  # Make sure this HTML file is in the 'templates' folder
+    return render_template('index.html')  # Make sure this HTML file is in the 'templates' folder
 
 if __name__ == "__main__":
     # Run the app on the local network
